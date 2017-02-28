3489cbfd7eb18af280a850baaf0085dcaf0db367
Marcos Petry <marcospetry@gmail.com>
2017-02-28 20:50:52 -0300

    separate searchbar component in a new file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyCUJlBCkjHnYVdGa5YqJ_wirK018j2ayMY';
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+  <div>
+    <SearchBar />
+  </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
