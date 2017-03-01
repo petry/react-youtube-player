3ebd6ef6dd6ba5e402a00771044ae14180f5b7b6
Marcos Petry <marcospetry@gmail.com>
2017-02-28 22:23:35 -0300

    show video information

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -3,11 +3,30 @@ import React, {Component} from 'react';
 class VideoListItem extends Component{
   constructor(props){
     super(props);
+    this.video = props.video;
   }
 
   render(){
+    // debugger;
+    // const imageUrl = this.props.video.snippet.thumbnails.default.url;
+    const imageUrl = this.video.snippet.thumbnails.default.url;
+    console.log(imageUrl);
     return(
-      <ul>video</ul>
+      <li className="list-group-item">
+        <div className="video-list media">
+          <div className="media-left">
+            <img className="media-object" src={imageUrl} />
+          </div>
+
+          <div className="media-body">
+            <div className="media-heading">
+              {this.video.snippet.title}
+            </div>
+
+          </div>
+        </div>
+
+      </li>
     )
   }
 };
