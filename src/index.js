99861d8f856b71a13e0a56c9e471b34bfed1f104
Marcos Petry <marcospetry@gmail.com>
2017-02-28 21:13:42 -0300

    fetch a search from youtube

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCUJlBCkjHnYVdGa5YqJ_wirK018j2ayMY';
 
-const App = () => {
-  return (
-  <div>
-    <SearchBar />
-  </div>
-  );
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'craft beers'}, (data) => {
+      this.setState({ videos: data});
+    });
+
+
+
+  }
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
